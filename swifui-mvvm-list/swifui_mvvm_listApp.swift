29460d2047f7839c8317e56efb064f965f97648a
Amini <ryans.mini28@gmail.com>
2025-04-22 12:36:38 +0700

    first commit

---
+++ swifui-mvvm-list/swifui_mvvm_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifui_mvvm_listApp.swift
+//  swifui-mvvm-list
+//
+//  Created by Amini on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct swifui_mvvm_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
